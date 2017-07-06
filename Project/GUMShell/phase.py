d122e9f432fc32ad17d6a2ec11c3f4208fdab207
Udo Gayer <gayer.udo@gmail.com>
2017-07-06 17:14:28 +0200

    Translated phase.f90 to python and tested it to yield the same results

--- Project/GUMShell/phase.py
+++ Project/GUMShell/phase.py
@@ -1,4 +1,41 @@
-def sort(N, a, phase):
+def sort(N, a):
     number = 0
+    p = 0
 
-    for i in range(1, N - 1, 1)
+    for i in range(0, N - 1, 1):
+        for j in range(i + 1, N, 1):
+            if(a[j] < a[i]):
+                number += 1
+                p = a[i]
+                a[i] = a[j]
+                a[j] = p
+
+    phase = (-1)**number
+
+    return phase
+
+def bandp(p, q, r, s, N, a):
+
+    point = 0
+    c = a
+
+    for i in range(0, N - 1, 1):
+        for j in range(i + 1, N, 1):
+            if r == a[i] and s == a[j]:
+                point = 1
+                a[i] = p
+                a[j] = q
+
+    for i in range(0, N - 1, 1):
+        for j in range(i + 1, N, 1):
+            if a[i] == a[j]:
+                point = 0
+
+    if point == 0:
+        phase = 0
+        b = c
+    elif point == 1:
+        phase = sort(N, a)
+        b = a
+
+    return phase, b
