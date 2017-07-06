fccb5effa164eed74f6c4d67d5dd7135deca44c0
Udo Gayer <gayer.udo@gmail.com>
2017-07-06 14:54:18 +0200

    Added file output and comments in mscheme.py

--- Project/GUMShell/mscheme.py
+++ Project/GUMShell/mscheme.py
@@ -6,24 +6,40 @@ J = 2
 M = 3
 T = 4
 
-N_PARTICLES = 3
+N_PARTICLES = 4
+################################################
+# Load file of available single-particle states
+################################################
 
-sp = np.loadtxt("./space/d5_2.txt")
+#sp = np.loadtxt("./space/d5_2.txt")
+sp = np.loadtxt("./space/0s1s2s3s.txt")
 
+# Determine number of unique single-particle (SP) states and number of shells (not needed at the moment)
 n_states = np.shape(sp)[0]
-n_shells = sp[n_states - 1][N]
+#n_shells = sp[n_states - 1][N]
 
 print("Number of SP states: ", n_states)
-print("Number of shells: ", n_shells)
+#print("Number of shells: ", n_shells)
 print()
 
+########################################
+# Initialize the list of basis states
+########################################
+
 basis = []
+# n_particle_state is a variable to store SP state before they are appended to the basis
 n_particle_state = [0]*N_PARTICLES
+# Initialize n_particle_state: The lowest state has the indices [1, 2, 3, 4, ...., N_PARTICLES]
 for i in range(N_PARTICLES):
     n_particle_state[i] = i + 1
+# Find the maximum possible state. It has the indices [n_states - N_PARTICLES, n_states - N_PARTICLES + 1, ..., n_states]
 max_state = [0]*N_PARTICLES
 for i in range(N_PARTICLES):
     max_state[N_PARTICLES - i - 1] = n_states - i
+ 
+####################################################
+# Calculate the basis for different numbers of particles. Some numbers have to be treated separately
+####################################################
 
 # Case N_PARTICLES == 1 has to be treated separately
 if N_PARTICLES == 1:
@@ -36,12 +52,16 @@ elif N_PARTICLES == n_states:
 
 
 # Any case with N_PARTICLES > 1 && N_PARTICLES < n_states
+# The algorithm works as follows (example with 3 particles)
+# 1) Start with the very first state (1,2,3)
+# 2) Keep increasing the last index until it is at the value of the maximum state 
+#   e.g., if the maximum state is (6,7,8), increase until (1,2,8)
+# 3) Increase the next-to-last index, lower the last index, and start from 1)
+#   e.g., after (1,2,8), go on with (1,3,4)
+# 4) Stop if you reach the maximum state
 else:
     while n_particle_state != max_state:
         basis.append(n_particle_state[:])
-    
-#        if n_particle_state[N_PARTICLES - 1] > 20:
-#            break
 
         for i in range(N_PARTICLES - 1):
             if n_particle_state[N_PARTICLES - i - 1] != max_state[N_PARTICLES - i - 1]:
@@ -61,22 +81,74 @@ else:
             basis.append(n_particle_state[:])
             break
 
-# Print basis states with positive M and create a list of M-values
+#####################################################
+# Print basis states with positive M and create a list of allowed J-values
+#####################################################
+M_values_positive = []
 M_values = []
 
+print("Listing basis states and their M quantum number")
+print()
+for i in range(N_PARTICLES):
+    print("M", i + 1, "\t", end="")
+print()
+
 for b in basis:
-    MJ = 0.
+    total_M = 0.
     for i in range(N_PARTICLES):
-        MJ += sp[b[i] - 1][M]
+        total_M += sp[b[i] - 1][M]
     
-    if MJ >= 0.:
+    M_values.append(total_M)
+    
+    if total_M >= 0.:
         for i in range(N_PARTICLES):
             print(b[i], "\t", end='')
     
-        print("M = ", MJ)
+        print("2*M = ", total_M)
         
-        M_values.append(MJ)
+        M_values_positive.append(total_M)
         
-print(M_values)
-print(M_values.count(5.))
+print()
 
+M_max = np.max(M_values_positive)
+M_min = np.min(M_values_positive)
+
+M_list = np.arange(M_min, M_max + 1., 1.)
+
+print("\tD(J)\td(M)")
+
+for m in M_list:
+    print(m, end="")
+    print("\t", end="")
+    if m < M_max:
+        print(M_values_positive.count(m) - M_values_positive.count(m + 2.), end="")
+    else:
+        print(M_values_positive.count(m), end="")
+    print("\t", end="")
+    print(M_values_positive.count(m), end="")
+    print()
+
+#################################
+# Write basis states to a file
+#################################
+
+# Write file header
+f = open("basis.txt", 'w')
+f.write("#")
+for i in range(N_PARTICLES):
+    f.write("I")
+    f.write(str(i + 1))
+    f.write("\t")
+    
+f.write("M\n")
+
+# Write states
+for i in range(np.shape(basis)[0]):
+    for bb in basis[i]:
+        f.write(str(bb))
+        f.write("\t")
+        
+    f.write(str(M_values[i]))
+    f.write("\n")
+    
+f.close()
