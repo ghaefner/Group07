Tested against NushellX, corrected error in mass factor, restricted loop. Another factor of ~1.5

Project/GUMShell/hamiltonian.py
@@ -1,7 +1,5 @@
 import numpy as np
-#from phase import bandp
-#from phase2 import Jcalc
-from phase_mod import bandp
+from phase_mod import adadaa
 import time
 
 HAMILTONIAN_DIR = "hamiltonian/"
@@ -28,53 +26,39 @@ def hamiltonian(basisfile, oneparticle, twoparticle, orbitals):
         n_particles = np.shape(basis)[1] - 1
 
     # One-body matrix elements
-    spe = np.loadtxt(oneparticle)
-#   n_spe = np.shape(spe)[0]
+    obme = np.loadtxt(oneparticle)
     # Two-body matrix elements
     tbme = np.loadtxt(twoparticle)
-#   n_tbme = np.shape(spe)[0]
-    # Single-particle states
-    sp = np.loadtxt(orbitals)
-    n_sp = np.shape(sp)[0]
-#   max_osc = np.max(sp[:,1])
-#   min_osc = np.min(sp[:,1])
-#   min_j = np.min(sp[:,2])
-#   max_m = np.max(sp[:,3])
-#   min_m = np.min(sp[:,3])
 
-    mass_factor = (18./(16.+n_particles))**(1./3.)
+    mass_factor = (18./(16.+n_particles))**(0.3)
 	
     START = time.time()
     
     H = np.zeros((basis_size, basis_size))
-                                                
+
     for alpha in range(basis_size):
         for tb in tbme:
             if (tb[2] in basis[alpha][:n_particles]) and (tb[3] in basis[alpha][:n_particles]):
             
-                phi, b = bandp(tb[0], tb[1], tb[2], tb[3], n_particles, list(basis[alpha][:n_particles]))
+                phi, b = adadaa(tb[0], tb[1], tb[2], tb[3], n_particles, list(basis[alpha][:n_particles]))
+                
                 if phi == 0:
                     continue
                 else:
-                    for beta in range(basis_size):
-                        if(len(basis[beta][:n_particles]) != len(b)):
-                            print("Error: hamiltonian.py: Vector dimensions not matching")
-                            exit(0)
-                        
-                        #if basis[beta][:n_particles] == b:
+                    for beta in range(0, alpha + 1):
                         if (basis[beta][:n_particles] == b).all():
                             H[alpha][beta] += phi*tb[4]*mass_factor
                             H[beta][alpha] = H[alpha][beta]
                             break
 
     for alpha in range(basis_size):
-        spme = 0.
+        spe = 0.
     
         for i in range(n_particles):
-            spme += spe[int(basis[alpha][i])-1][4]
+            spe += obme[int(basis[alpha][i])-1][4]
     
-        H[alpha][alpha] += spme
-    spme = 0.
+        H[alpha][alpha] += spe
+    spe = 0.
     
     STOP = time.time()
     